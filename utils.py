5e9759440940582760cd78ef132ea04ab21b9b79
combo <combo@taou.com>
2021-07-01 17:54:07 +0800

    add day2 work

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ def get_list_data_from_file(file_name, parser=str, sep='\n') -> list:
     sections = open(file_path).read().rstrip().split(sep)
     return [parser(section) for section in sections]
 
-
-
-
+def quantify(iterable, pred=bool) -> int:
+    "Count the number of items in iterable for which pred is true."
+    return sum(1 for item in iterable if pred(item))
